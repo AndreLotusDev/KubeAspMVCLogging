4edb3755c684f4c0d8ac84881a41714cd3c0b4f2
Andre Soares Gomes <andrsoares953@yahoo.com>
2023-01-03 16:25:54 -0300

    Add project files.

---
+++ TempMVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TempMVC.Models;
+
+namespace TempMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["words"] = RandomWords(3);
+
+            return View();
+        }
+
+        private List<string> RandomWords(int quantity)
+        {
+            var wordList = System.IO.File.ReadLines(@"./RandomWords.txt").ToList();
+            wordList = ShuffleAndTakeTen(wordList);
+
+            var messageToSend = $"Words being listed: {string.Join(" ", wordList.Select(s => s))}";
+            
+            Console.WriteLine($"Produced by console: {messageToSend}");
+            _logger.LogInformation($"Produced by serilog: {messageToSend}");
+
+            return wordList.ToList();
+        }
+
+        private List<string> ShuffleAndTakeTen(List<string> words)
+        {
+            var shuffledWords = words.OrderBy(a => Guid.NewGuid()).Take(10).ToList();
+
+            return shuffledWords;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TempMVC/Program.cs
@@ -0,0 +1,43 @@
+using Serilog;
+using TempMVC;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    SerilogExtension.AddSerilogApi(builder.Configuration);
+    builder.Host.UseSerilog(Log.Logger);
+
+    builder.Services.AddControllersWithViews();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.Information("Server Shutting down...");
+    Log.CloseAndFlush();
+}
